5720277be11876640e7c8b6a6d3e5bd6895c9c28
HaoYin <38425379+H-Yin@users.noreply.github.com>
2018-08-24 20:49:29 +0800

    Create 62. Unique Paths.cpp

---
+++ Array/62. Unique Paths.cpp
@@ -0,0 +1,39 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+How many possible unique paths are there?
+Note: m and n will be at most 100.
+
+Example 1:
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+*/
+// DP
+class Solution {
+public:
+    unsigned long long uniquePaths(int m, int n) {
+        vector<vector<long long>> path;
+        path.resize(m, vector<long long>(n));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 or j == 0){
+                    path[i][j] = 1ULL;
+                }else {
+                    path[i][j] = path[i-1][j] + path[i][j-1];
+                }
+            }
+        }
+        cout<<path[m-1][n-1];
+        return path[m-1][n-1];
+    }
+};
+// Math
