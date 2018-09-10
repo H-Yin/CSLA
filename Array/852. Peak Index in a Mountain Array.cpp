a55d17b67fbb0a7e105cfca7fd801a7f519023ac
HaoYin <38425379+H-Yin@users.noreply.github.com>
2018-09-11 00:13:09 +0800

    Update 852. Peak Index in a Mountain Array.cpp

--- Array/852. Peak Index in a Mountain Array.cpp
+++ Array/852. Peak Index in a Mountain Array.cpp
@@ -34,3 +34,24 @@ public:
         return i;
     }
 };
+/* 
+// better
+class Solution {
+public:
+    // binary search
+    int peakIndexInMountainArray(vector<int>& A) {
+        int low = 0, high = A.size()-1, mid;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(A[mid]>A[mid+1] && A[mid] > A[mid-1]){
+                return mid;
+            }else if(A[mid] < A[mid+1]){
+                low = mid + 1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return mid;
+    }
+};
+*/
